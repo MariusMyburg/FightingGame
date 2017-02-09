07ce221b0ec3f0239500a9d01ba61ab6cf71944c
Marius Myburg <mariusmyburg@gmail.com>
2017-02-10 00:24:29 +0200

    Nothing.

--- Source/FightGame/FightGame.Build.cs
+++ Source/FightGame/FightGame.Build.cs
@@ -7,6 +7,10 @@ public class FightGame : ModuleRules
 	public FightGame(TargetInfo Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		
+		PublicDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemUtils" });
+		
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -14,7 +18,7 @@ public class FightGame : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 		
